736f299778c5f63bab1d45cf36dd203e327bfa22
rumusa <19166472+rumusa@users.noreply.github.com>
2022-08-07 23:48:57 +0200

    Add user module, verify user email address

--- echti/app.py
+++ echti/app.py
@@ -1,2 +1,8 @@
+from echti.auth.user import User
+
 def run():
-    print("Test")
+    user1 = User("", "")
+    print("============================")
+    print(user1.email)
+    print(user1.password)
+    print("============================")
---
+++ echti/auth/user.py
@@ -0,0 +1,29 @@
+import hashlib
+import re
+
+class User:
+
+    _email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        if (re.fullmatch(self._email_regex, email)):
+            self._email = email
+        else:
+            raise ValueError(f'{email} email address is not correct. Please enter a correct email address.')
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
