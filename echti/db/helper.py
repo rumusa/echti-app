3b9f6a75e2c3df618324b3660b6e9a37362df988
rumusa <19166472+rumusa@users.noreply.github.com>
2022-09-26 21:22:43 +0200

    Refactor echti/helper.py

--- echti/db/helper.py
+++ echti/db/helper.py
@@ -1,10 +1,8 @@
-from distutils.command.config import config
 import mysql.connector
 import echti.config
 
+
 class DBHelper(object):
-    _conn = None
-    cursor = None
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
@@ -12,20 +10,19 @@ class DBHelper(object):
         return cls.instance
 
     def __init__(self):
-        if self._conn is None:
-            self._conn = mysql.connector.connect(
-                host = "localhost",
-                user = echti.config.mysql_username,
-                passwd = echti.config.mysql_password
-            )
-        self.cursor = self._conn.cursor()
+        self.conn = mysql.connector.connect(
+            host="localhost",
+            user=echti.config.mysql_username,
+            passwd=echti.config.mysql_password
+        )
+        self.cursor = self.conn.cursor()
         self.cursor.execute("SHOW DATABASES")
-        # for db_name in self.cursor:
-            # print(db_name)
-        if echti.config.db_name not in self.cursor:
-            self.cursor.execute("CREATE DATABASE " + echti.config.db_name)
-        
+        for db_name in self.cursor:
+            print(db_name)
+        # if echti.config.db_name not in self.cursor:
+        #     self.cursor.execute("CREATE DATABASE " + echti.config.db_name)
+
     def __del__(self):
-        if self._conn is not None:
-            self._conn.close()
-            self._conn = None
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
